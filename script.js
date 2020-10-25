3af7b46c9b10330de4494416f9b8eb51d3f54bab
Caitlin White <caitlinritawhite@gmail.com>
2020-10-25 15:35:58 -0400

    Created About and Portfolio Pages

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-$("#about-me").click(function (event) {
-    event.preventDefault();
-    let userInput = $("#search-input").val();
-    document.getElementById("recipeList").style.display = "block";
-    document.getElementById("favorite-results").style.display = "none";
-    document.getElementById("review-text").style.display = "none";
-    document.getElementById("faves").style.display = "inline";
-    document.getElementById("add").style.display = "inline";
-    document.getElementById("add").style.display = "inline";
+// $("#about-me").click(function (event) {
+//     event.preventDefault();
   
-    ajaxCallSearch(userInput);
-  });
+//     document.getElementById("about-info").style.display = "block";
+//     document.getElementById("description").style.display = "none";
+//     document.getElementById("buttons").style.display = "none";
+//   });
+
+  function showAboutMe(){
+    document.getElementById("about-info").style.display = "block";
+    document.getElementById("description").style.display = "none";
+    document.getElementById("name").style.display="none";
+    document.getElementById("button1").style.display = "none";
+    document.getElementById("button2").style.display = "inline";
+    document.getElementById("button3").style.display = "inline";
+    document.getElementById("caitlin-photo").style.display="block";
+    document.getElementById("portfolio-section").style.display="none";
+    document.getElementById("home-button").style.display="block";
+    }
+
+  function showPortfolio(){
+    document.getElementById("portfolio-section").style.display="block";
+    document.getElementById("about-info").style.display="none";
+    document.getElementById("caitlin-photo").style.display="none";
+    document.getElementById("description").style.display = "none";
+    document.getElementById("name").style.display="none";
+    document.getElementById("button2").style.display="none";
+    document.getElementById("button1").style.display="inline";
+    document.getElementById("button3").style.display = "inline";
+    document.getElementById("home-button").style.display="block";
+
+  }
+
+  function homeButton(){
+    document.getElementById("portfolio-secion").style.display="none";
+    document.getElementById("about-info").style.display="none";
+    document.getElementById("caitlin-photo").style.display="none";
+    document.getElementById("description").style.display="block";
+    document.getElementById("name").style.display="block";
+    document.getElementById("home-button").style.display="none";
+    document.getElementById("button2").style.display="inline";
+    document.getElementById("button1").style.display="inline";
+    document.getElementById("button3").style.display = "inline";
+
+  }
